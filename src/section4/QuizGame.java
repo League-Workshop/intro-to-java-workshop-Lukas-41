b5b268bce91b9d7b1b915c434ddac7234217046a
League Student <league@iMac-9.attlocal.net>
2019-10-19 12:57:02 -0700

    quiz game
    i made some questions in quiz game and got them all right yay! and scared everyone half to death when i played a creepy noise

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,53 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
+	private static final String  Jo = null;
+
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String answer=JOptionPane.showInputDialog("what is 20+70");
 		// 3.  Use an if statement to check if their answer is correct
+		if(answer.equals("90")){ 
+			
+			JOptionPane.showMessageDialog( null,"correct");
+			score =score+1;
+			
 		
+		
+		}else {
+			JOptionPane.showMessageDialog( null,"incorrect");
+
+			
+			
+					}
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+	
+	
+	if(answer.equals("150"));
 	}
-}
+			JOptionPane.showMessageDialog( null,"correct");
+			score =score+2;
+			
+		}else {
+			JOptionPane.showMessageDialog( null,"you are so dumb");
+			
+	} }
+		
+			
+	
+		
+
+		
+		
+		
+		
+	
+
